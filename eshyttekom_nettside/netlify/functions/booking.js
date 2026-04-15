abdd590e0284db1494ae59011dbd2d0842403bef
Akos Nagy <akosaksel@gmail.com>
2026-04-15 10:40:59 +0200

    ip basert relay

--- eshyttekom_nettside/netlify/functions/booking.js
+++ eshyttekom_nettside/netlify/functions/booking.js
@@ -133,10 +133,6 @@ Kommentar: ${kommentar || "-"}
     host: "smtp-relay.gmail.com",
     port: 587,
     secure: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
     tls: {
       rejectUnauthorized: false,
     },
